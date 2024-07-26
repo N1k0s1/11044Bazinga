b951041fad7c868379bd8c4133d0423ba68747a6
Niko <128133376+N1k0s1@users.noreply.github.com>
2024-07-27 10:42:46 +1200

    Initial commit

---
+++ include/robot-config.h
@@ -0,0 +1,17 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern drivetrain Drivetrain;
+extern controller Controller1;
+extern motor DonutWinch;
+extern pneumatics wings1;
+extern motor sparered;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
